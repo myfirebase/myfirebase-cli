ea2926e3d9c0ce1a437cf2cbe549a0e0377ac72f
Amrani Houssain <amranidev@gmail.com>
2017-09-10 16:46:58 +0100

    update new.js

--- bin/new.js
+++ bin/new.js
@@ -2,7 +2,6 @@
 
 var download = require('download-git-repo')
 var program = require('commander')
-var exists = require('fs').existsSync
 var path = require('path')
 var ora = require('ora')
 var home = require('user-home')
@@ -10,7 +9,11 @@ var tildify = require('tildify')
 var chalk = require('chalk')
 var inquirer = require('inquirer')
 var figlet = require('figlet')
+var shell = require("shelljs")
+var ncp = require("ncp").ncp
+
 program
+    .version(require('./../package').version)
     .usage('[project-name]')
     .option('-c, --clone', 'use git clone')
 
@@ -19,10 +22,14 @@ program
  */
 
 program.on('--help', function() {
+    console.log(chalk.green(figlet.textSync('Myfirease-cli', { horizontalLayout: 'full' })))
     console.log('  Example:')
     console.log()
     console.log(chalk.gray('    # create a new project with Myfirebase'))
-    console.log('    $ myfirebase-new my-project')
+    console.log('    $ myfirebase <project-name>')
+    console.log()
+    console.log(chalk.gray('    # create a new component in your project'))
+    console.log('    $ myfirebase new-component <component-name>')
     console.log()
 })
 
@@ -30,8 +37,6 @@ function help() {
     program.parse(process.argv)
     if (program.args.length < 1) return program.help()
 }
-
-console.log(chalk.green(figlet.textSync('Myfirease-cli', { horizontalLayout: 'full' })))
 
 help()
 
@@ -44,19 +49,33 @@ var inPlace = !rawName || rawName === '.'
 var name = inPlace ? path.relative('../', process.cwd()) : rawName
 var to = path.resolve(rawName || '.')
 var clone = program.clone || false
+var templateDir = path.join(__dirname, './../lib/scaffold')
 
 /**
  * Run.
  */
 function run() {
-    var spinner = ora('downloading project')
-    spinner.start()
-    download("myfirebase/myfirebase", to, { clone: clone }, function(err) {
-        spinner.stop()
-        if (!err) { console.log(chalk.green(name + " has been successfully created.")) } else {
-            console.log(chalk.red('Failed to download repo : ' + err.message.trim()))
-        }
-    })
+    if (program.args[0] == 'new-component') {
+        var componentName = program.args[1] + ".vue"
+        var componentDir = path.resolve("src/components/" + componentName)
+        ncp(templateDir + "/component.vue", componentDir, function(error) {
+            if (error) {
+                console.log(chalk.red(error))
+                return 1
+            }
+            console.log(chalk.green("a new component is successfully created"));
+        })
+    } else {
+        var spinner = ora('downloading project')
+        spinner.start()
+        download("myfirebase/myfirebase", to, { clone: clone }, function(err) {
+            spinner.stop()
+            if (!err) { console.log(chalk.green(name + " has been successfully created.")) } else {
+                console.log(chalk.red('Failed to download repo : ' + err.message.trim()))
+            }
+        })
+    }
 }
 
+// run commmand
 run()
