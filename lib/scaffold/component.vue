643687b88e02f80457301653f2a6c58bdb2237c5
Amrani Houssain <amranidev@gmail.com>
2017-09-10 16:46:00 +0100

    create component template

---
+++ lib/scaffold/component.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <!-- your markup here -->
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("component is ready!!")
+    },
+    data() {
+        return {
+            // models
+        }
+    },
+    methods: {
+        // methods
+    }
+}
+</script>
+
+<style scoped>
+    /*stylesheets*/
+</style>
